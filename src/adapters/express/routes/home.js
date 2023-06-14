ecacf17ba72a42e82fb5de5a083fcb4759fda4a5
leofuna <leo.funa@gmail.com>
2023-06-14 15:23:35 -0300

    feat: home api route

---
+++ src/adapters/express/routes/home.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function (req, res) {
+  res.send(`get user/ </br>
+  get users/ </br>
+  post users/ </br>
+  delete users/ </br>
+  put users/ </br>
+  `);
+});
+
+module.exports = router;
