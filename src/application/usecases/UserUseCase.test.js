d3ccef7e72613e26d2a23c5b330a875d910fb3a3
leofuna <leo.funa@gmail.com>
2023-06-16 17:53:21 -0300

    test: createUser test from user usecase

--- src/application/usecases/UserUseCase.test.js
+++ src/application/usecases/UserUseCase.test.js
@@ -15,19 +15,27 @@ describe('Tests UserUseCase class', () => {
     });
     let userRepositoryInstance;
     let userAccessRepositoryInstance;
-
+    let userUseCaseInstance;
     beforeEach(() => {
       userRepositoryInstance = new AbstractUserRepository();
       userAccessRepositoryInstance = new AbstractUserAccessRepository();
+      userUseCaseInstance = new UserUseCase(
+        userRepositoryInstance,
+        userAccessRepositoryInstance
+      );
       userRepositoryInstance.getUser.mockReturnValue(mockedUser);
       userAccessRepositoryInstance.createNewAccess.mockReturnValue('Created');
     });
 
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
     it('has user with valid data', async () => {
-      const userFound = await new UserUseCase(
-        userRepositoryInstance,
-        userAccessRepositoryInstance
-      ).getUser(mockedUser.name, 'name');
+      const userFound = await userUseCaseInstance.getUser(
+        mockedUser.name,
+        'name'
+      );
 
       expect(userFound).toEqual(mockedUser);
     });
@@ -35,19 +43,107 @@ describe('Tests UserUseCase class', () => {
       userRepositoryInstance.getUser.mockReturnValue(null);
 
       await expect(
-        new UserUseCase(
-          userRepositoryInstance,
-          userAccessRepositoryInstance
-        ).getUser('MockedName', 'name')
+        userUseCaseInstance.getUser('MockedName', 'name')
       ).rejects.toThrow('Not Found Error');
     });
     it('has invalid name', async () => {
+      await expect(userUseCaseInstance.getUser('', 'name')).rejects.toThrow(
+        'Unique Key is required'
+      );
+    });
+  });
+
+  describe('createUser method when', () => {
+    const mockedUser = new User({
+      id: 1,
+      name: 'MockedName',
+      job: 'MockedJob',
+    });
+    let userRepositoryInstance;
+    let userAccessRepositoryInstance;
+    let userUseCaseInstance;
+
+    beforeEach(() => {
+      userRepositoryInstance = new AbstractUserRepository();
+      userAccessRepositoryInstance = new AbstractUserAccessRepository();
+      userUseCaseInstance = new UserUseCase(
+        userRepositoryInstance,
+        userAccessRepositoryInstance
+      );
+      userRepositoryInstance.createUser.mockImplementation((user) => user);
+      jest
+        .spyOn(userUseCaseInstance, '_checkIfUserAlreadyExists')
+        .mockReturnValue(undefined);
+      jest.spyOn(userUseCaseInstance, '_generateNewUserId').mockReturnValue(1);
+    });
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it('has valid data, create a User', async () => {
+      const createdUser = await userUseCaseInstance.createUser({
+        name: 'MockedName',
+        job: 'MockedJob',
+      });
+
+      expect(createdUser).toEqual(mockedUser);
+    });
+    it('has no name, throws error', async () => {
       await expect(
-        new UserUseCase(
-          userRepositoryInstance,
-          userAccessRepositoryInstance
-        ).getUser('', 'name')
-      ).rejects.toThrow('Unique Key is required');
+        userUseCaseInstance.createUser({ job: 'MockedJob' })
+      ).rejects.toThrow('Job and Name are required!');
+    });
+    it('has valid data but name already exists, throws error', async () => {
+      jest
+        .spyOn(userUseCaseInstance, '_checkIfUserAlreadyExists')
+        .mockImplementation(() => {
+          throw new Error('Cant create this user, name already exists!');
+        });
+
+      await expect(
+        userUseCaseInstance.createUser({
+          name: 'MockedName',
+          job: 'MockedJob',
+        })
+      ).rejects.toThrow('Cant create this user, name already exists!');
+    });
+  });
+
+  describe('_checkIfUserAlreadyExists method when', () => {
+    let userRepositoryInstance;
+    let userAccessRepositoryInstance;
+    let userUseCaseInstance;
+    beforeEach(() => {
+      userRepositoryInstance = new AbstractUserRepository();
+      userAccessRepositoryInstance = new AbstractUserAccessRepository();
+      userUseCaseInstance = new UserUseCase(
+        userRepositoryInstance,
+        userAccessRepositoryInstance
+      );
+      userRepositoryInstance.getUser.mockReturnValue(null);
+    });
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+    it('has valid unique name, resolves', async () => {
+      const uniqueUserName = 'John Doe';
+
+      await expect(
+        userUseCaseInstance._checkIfUserAlreadyExists(uniqueUserName)
+      ).resolves.toBe(undefined);
+    });
+    it('has valid non-unique name, throws error', async () => {
+      const nonUniqueName = 'John Doe';
+
+      userRepositoryInstance.getUser.mockReturnValue(
+        new User({ id: 1, name: nonUniqueName, job: 'fakeJob' })
+      );
+
+      await expect(
+        userUseCaseInstance._checkIfUserAlreadyExists(nonUniqueName)
+      ).rejects.toThrow('Cant create this user, name already exists!');
     });
   });
 });
